c21e9da92188ee5ef7c1244d9ab7cbb0b921731c
Robert Matundu <matundukabamba@gmail.com>
2024-01-24 11:32:26 +0100

    projet init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  
+  return (
+    <>
+      <div className=" text-red-800 text-7xl text-center p-40 font-extrabold bg-yellow-500">Teste</div>
+    </>
+  )
+}
+
+export default App
