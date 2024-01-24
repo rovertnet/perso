cc5682bf2e34f9684210de482a9acd1269367b9f
Robert Matundu <matundukabamba@gmail.com>
2024-01-25 00:30:53 +0100

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ function App() {
   
   return (
     <>
-      <div className=" bg-gray-400"></div>
+      <div className=" bg-gray-200 md:px-24 px-20 md:py-24 py-20">
+        <Login />
+      </div>
     </>
-  )
+  );
 }
 
 export default App
--- src/composant/Login.jsx
+++ src/composant/Login.jsx
@@ -2,7 +2,7 @@
 function Login() {
   return (
     <>
-      <div className="md:mx-96 mx-80 md:px-8 px-6 md:h-full h-full rounded-lg bg-slate-100 md:my-60 my-56 shadow-md">
+      <div className="md:mx-96 mx-80 md:px-8 px-6 md:h-full h-full rounded-lg bg-white md:my-60 my-56 shadow-md md:py-14 py-12">
         <div>
           <h1 className=" text-slate-900 md:text-3xl text-2xl"> Log in</h1>
           <div className="flex justify-center items-center gap-5">
@@ -14,7 +14,8 @@ function Login() {
           <div className="">
             <form action="">
               <div className="">
-                
+                <input type="text" placeholder=" Nom complet" className="" />
+                <input type="text" placeholder=" Nom complet" className="" />
               </div>
             </form>
           </div>
