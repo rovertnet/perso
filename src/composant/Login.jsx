f5c5440e1f300ee95ef5339e1206bf2a43556097
Robert Matundu <matundukabamba@gmail.com>
2024-01-25 00:20:17 +0100

    add component

--- src/composant/Login.jsx
+++ src/composant/Login.jsx
@@ -2,7 +2,7 @@
 function Login() {
   return (
     <>
-      <div className="md:mx-32 mx-28 md:px-8 px-6 md:h-full h-full rounded-lg bg-slate-200 flex justify-center md:my-60 my-56 shadow-md">
+      <div className="md:mx-96 mx-80 md:px-8 px-6 md:h-full h-full rounded-lg bg-slate-100 md:my-60 my-56 shadow-md">
         <div>
           <h1 className=" text-slate-900 md:text-3xl text-2xl"> Log in</h1>
           <div className="flex justify-center items-center gap-5">
@@ -13,7 +13,9 @@ function Login() {
           </div>
           <div className="">
             <form action="">
-              
+              <div className="">
+                
+              </div>
             </form>
           </div>
         </div>
