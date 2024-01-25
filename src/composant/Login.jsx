10e75ab8bbdd56b067382b8686b0fe4747ee8d5e
Robert Matundu <matundukabamba@gmail.com>
2024-01-25 12:34:16 +0100

    update

--- src/composant/Login.jsx
+++ src/composant/Login.jsx
@@ -4,16 +4,19 @@ function Login() {
     <>
       <div className="md:mx-96 mx-80 md:px-8 px-6 md:h-full h-full rounded-lg bg-white md:my-60 my-56 shadow-md md:py-14 py-12">
         <div>
-          <h1 className=" text-slate-900 md:text-3xl text-2xl"> Log in</h1>
+          <h1 className=" text-slate-900 md:text-3xl text-2xl md:mb-14 mb-11">
+            {" "}
+            Log in
+          </h1>
           <div className="flex justify-center items-center gap-5">
             <a href="">R</a>
             <a href="">R</a>
             <a href="">R</a>
             <a href="">R</a>
           </div>
-          <div className="">
+          <div className=" ">
             <form action="">
-              <div className="">
+              <div className=" flex flex-col md:py-10 py-8">
                 <input type="text" placeholder=" Nom complet" className="" />
                 <input type="text" placeholder=" Nom complet" className="" />
               </div>
