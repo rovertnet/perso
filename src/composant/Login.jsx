935a0dacf1639ca8fc100f01d639a63f436351d4
Robert Matundu <matundukabamba@gmail.com>
2024-01-25 15:15:39 +0100

    update

--- src/composant/Login.jsx
+++ src/composant/Login.jsx
@@ -1,3 +1,4 @@
+import { IoLogoGoogle } from "react-icons/io5";
 
 function Login() {
   return (
@@ -9,17 +10,35 @@ function Login() {
             Sign In Now
           </h1>
           <div className="flex justify-center items-center gap-5">
-            <a href="" className=" bg-slate-100 py-2 px-6 rounded-xl">R</a>
-            <a href="" className=" bg-slate-100 py-2 px-6 rounded-xl">R</a>
-            <a href="" className=" bg-slate-100 py-2 px-6 rounded-xl">R</a>
-            <a href="" className=" bg-slate-100 py-2 px-6 rounded-xl">R</a>
+            <a href="" className=" bg-slate-100 py-2 px-6 rounded-xl">
+              <IoLogoGoogle className=" text-blue-700 font-extrabold text-xl" />
+            </a>
+            <a href="" className=" bg-slate-100 py-2 px-6 rounded-xl">
+              R
+            </a>
+            <a href="" className=" bg-slate-100 py-2 px-6 rounded-xl">
+              R
+            </a>
+            <a href="" className=" bg-slate-100 py-2 px-6 rounded-xl">
+              R
+            </a>
           </div>
           <div className=" ">
             <form action="">
               <div className=" flex flex-col md:py-10 py-8">
-                <input type="text" placeholder=" Nom complet" className="bg-slate-100 outline outline-0 focus:outline-0 rounded-xl md:py-4 py-2 md:px-6 px-3 md:mb-6 mb-3 md:w-full w-full" />
-                <input type="text" placeholder=" Nom complet" className="bg-slate-100 outline outline-0 focus:outline-0 rounded-xl md:py-4 py-2 md:px-6 px-3 md:mb-6 mb-3 md:w-full w-full" />
-                <button className=" bg-blue-600 py-2 px-20 text-2xl font-bold text-white rounded-xl">Sign In</button>
+                <input
+                  type="text"
+                  placeholder=" Nom complet"
+                  className="bg-slate-100 outline outline-0 focus:outline-0 rounded-xl md:py-4 py-2 md:px-6 px-3 md:mb-6 mb-3 md:w-full w-full"
+                />
+                <input
+                  type="text"
+                  placeholder=" Nom complet"
+                  className="bg-slate-100 outline outline-0 focus:outline-0 rounded-xl md:py-4 py-2 md:px-6 px-3 md:mb-6 mb-3 md:w-full w-full"
+                />
+                <button className=" bg-blue-600 py-2 px-20 text-2xl font-bold text-white rounded-xl">
+                  Sign In
+                </button>
               </div>
             </form>
           </div>
@@ -29,4 +48,4 @@ function Login() {
   );
 }
 
-export default Login
+export default Login;
