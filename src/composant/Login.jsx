d44d64a5791add0344672b446398558c450b4a0e
Robert Matundu <matundukabamba@gmail.com>
2024-01-25 13:32:37 +0100

    update

--- src/composant/Login.jsx
+++ src/composant/Login.jsx
@@ -2,11 +2,11 @@
 function Login() {
   return (
     <>
-      <div className="bloc1 md:mx-96 mx-80 md:px-16 px-14 md:h-full h-full rounded-x6 bg-white md:my-60 my-56 shadow-md md:py-14 py-12">
+      <div className="bloc1 md:mx-96 mx-80 md:px-16 px-14 md:h-full h-full rounded-xl bg-white md:my-60 my-56 shadow-md md:py-14 py-12">
         <div>
-          <h1 className=" text-slate-900 md:text-3xl text-2xl md:mb-14 mb-11 text-center">
+          <h1 className=" text-slate-500 md:text-3xl text-2xl md:mb-14 mb-11 text-center font-extrabold">
             {" "}
-            Log in
+            Sign In Now
           </h1>
           <div className="flex justify-center items-center gap-5">
             <a href="" className=" bg-slate-100 py-2 px-6 rounded-xl">R</a>
@@ -19,7 +19,7 @@ function Login() {
               <div className=" flex flex-col md:py-10 py-8">
                 <input type="text" placeholder=" Nom complet" className="bg-slate-100 outline outline-0 focus:outline-0 rounded-xl md:py-4 py-2 md:px-6 px-3 md:mb-6 mb-3 md:w-full w-full" />
                 <input type="text" placeholder=" Nom complet" className="bg-slate-100 outline outline-0 focus:outline-0 rounded-xl md:py-4 py-2 md:px-6 px-3 md:mb-6 mb-3 md:w-full w-full" />
-                <button className=" bg-blue">Sign In</button>
+                <button className=" bg-blue-600 py-2 px-20 text-2xl font-bold text-white rounded-xl">Sign In</button>
               </div>
             </form>
           </div>
