381345b405cdd731c969974d561477fcb2b3dbbf
Robert Matundu <matundukabamba@gmail.com>
2024-01-27 23:26:57 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
-import Slider from "./composant/Slider";
+
 
 function App() {
   
   return (
     <>
       <div className=" bg-gray-200 md:my-10 my-8 md:py-10 py-8">
-        <Slider />
+        
       </div>
     </>
   );
