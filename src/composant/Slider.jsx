173d55421e93e0b69be0d9aa24c5e21537314a01
Robert Matundu <matundukabamba@gmail.com>
2024-01-27 23:25:28 +0100

    update

--- src/composant/Slider.jsx
+++ src/composant/Slider.jsx
@@ -1,32 +1,9 @@
-import { Carousel } from "flowbite-react";
+
 
 function Slider() {
   return (
     <>
-      <div className="grid h-56 grid-cols-2 gap-4 md:h-64 xl:h-80 2xl:h-96">
-        <Carousel indicators={false}>
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-            alt="..."
-          />
-        </Carousel>
-      </div>
+      
     </>
   );
 }
