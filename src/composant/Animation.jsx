0af95c80322cb5edcd82388471156276fd454044
Robert Matundu <matundukabamba@gmail.com>
2024-01-28 17:30:10 +0100

    update

--- src/composant/Animation.jsx
+++ src/composant/Animation.jsx
@@ -8,6 +8,15 @@ function Animation() {
   useLayoutEffect(()  => {
     let ctx = gsap.config(() => {
       const t1 = gsap.timeline()
+      t1.from("#intro-slider", {
+        xPercent: "-100",
+        duration: 1.3,
+        delay: 0.3,
+      }).from(["#title-1", "#title-2", "#title-3"], {
+        opacity: 0,
+        y : "+=30",
+        stagger : 0.5
+      });
     }, compte)
 
     return () => ctx.revert()
@@ -15,7 +24,7 @@ function Animation() {
 
   return (
     <>
-      <div className="relative">
+      <div className="relative" ref={compte}>
         <div id="intro-slider" className=" h-screen p-10 bg-gray-100 absolute right-0 top-0 z-10 w-full flex flex-col gap-10 tracking-tight text-8xl font-bold">
           <h1 id="title-1" className=" text-8xl font-bold">Je suis le premier texte</h1>
           <h1 id="title-2" className=" text-8xl font-bold">Je suis le premier texte</h1>
