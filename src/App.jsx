49347f6581b6d75ca1b44cf14e69a49b37d96fb3
Robert Matundu <matundukabamba@gmail.com>
2024-01-29 13:46:48 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,6 @@ function App() {
   
   return (
     <>
-      {/* <div className=" bg-gray-200 md:my-10 my-8 md:py-10 py-8">
-        <GridImg />
-      </div> */}
-
       <Modale />
     </>
   );
