118773a58aceae9ff838df98c72a7a61f651f6e5
Robert Matundu <matundukabamba@gmail.com>
2024-01-30 15:51:18 +0100

    update

--- src/composant/Devis.jsx
+++ src/composant/Devis.jsx
@@ -1,1 +1,15 @@
-rfce
+
+function Devis() {
+  return (
+    <>
+      <div className="md:px-14 px-10 md:mx-auto">
+        <button className=""></button>
+        <div className="">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Devis
