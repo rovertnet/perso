90d175e456d02a241a2e8263badba58623c5317c
Robert Matundu <matundukabamba@gmail.com>
2024-01-31 23:06:28 +0100

    update

--- src/composant/Devis.jsx
+++ src/composant/Devis.jsx
@@ -1,38 +1,20 @@
-import { Button, Modal } from "flowbite-react";
-import { useState } from "react";
 
 function Devis() {
-  const [openModal, setOpenModal] = useState(false);
   return (
     <>
-      <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
-      <Modal show={openModal} onClose={() => setOpenModal(false)}>
-        <Modal.Header>Terms of Service</Modal.Header>
-        <Modal.Body>
-          <div className="space-y-6">
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new
-              consumer privacy laws for its citizens, companies around the world
-              are updating their terms of service agreements to comply.
-            </p>
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.)
-              goes into effect on May 25 and is meant to ensure a common set of
-              data rights in the European Union. It requires organizations to
-              notify users as soon as possible of high-risk data breaches that
-              could personally affect them.
-            </p>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={() => setOpenModal(false)}>I accept</Button>
-          <Button color="gray" onClick={() => setOpenModal(false)}>
-            Decline
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <div className="md:px-14 px-10 md:mx-12 mx-10 rounded-md bg-red-400">
+        <button className=""></button>
+        <div className=" md:my-10 my-5 bg-white opacity-40 rou md:px-10 px-5 md:py-10 py-5">
+            {/* Formulaire */}
+            <form action="">
+              <div className="flex justify-center items-center gap-10">
+                <input type="text" placeholder="Votre nom" className="" />
+              </div>
+            </form>
+        </div>
+      </div>
     </>
-  );
+  )
 }
 
-export default Devis;
+export default Devis
