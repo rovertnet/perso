4d26f10d3295ef890b666fd7931c27094369d555
Robert Matundu <matundukabamba@gmail.com>
2024-01-31 23:14:13 +0100

    update

--- src/composant/Devis.jsx
+++ src/composant/Devis.jsx
@@ -4,11 +4,11 @@ function Devis() {
     <>
       <div className="md:px-14 px-10 md:mx-12 mx-10 rounded-md bg-red-400">
         <button className=""></button>
-        <div className=" md:my-10 my-5 bg-white opacity-40 rou md:px-10 px-5 md:py-10 py-5">
+        <div className=" md:my-10 my-10 bg-white opacity-60 rounded-md md:px-10 px-5 md:py-10 py-5">
             {/* Formulaire */}
             <form action="">
               <div className="flex justify-center items-center gap-10">
-                <input type="text" placeholder="Votre nom" className="" />
+                <input type="text" placeholder="Votre nom" className=" md:px-5 px-3 md:py-2 py-2 bg-gray-300 text-white outline outline-0 focus:outline-0" />
               </div>
             </form>
         </div>
