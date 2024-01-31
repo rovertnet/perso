434b75a824bb1efe477ad0217daac25e9e12ba35
Robert Matundu <matundukabamba@gmail.com>
2024-01-31 23:20:25 +0100

    update

--- src/composant/Devis.jsx
+++ src/composant/Devis.jsx
@@ -2,19 +2,29 @@
 function Devis() {
   return (
     <>
-      <div className="md:px-14 px-10 md:mx-12 mx-10 rounded-md bg-red-400">
+      <div className="md:px-14 px-10 md:mx-12 mx-10 rounded-md bg-slate-700">
         <button className=""></button>
-        <div className=" md:my-10 my-10 bg-white opacity-60 rounded-md md:px-10 px-5 md:py-10 py-5">
-            {/* Formulaire */}
-            <form action="">
-              <div className="flex justify-center items-center gap-10">
-                <input type="text" placeholder="Votre nom" className=" md:px-5 px-3 md:py-2 py-2 bg-gray-300 text-white outline outline-0 focus:outline-0" />
-              </div>
-            </form>
+        <div className=" mb-10 bg-white opacity-60 rounded-md md:px-10 px-5 md:py-10 py-5 ">
+          {/* Formulaire */}
+          <form action="">
+            <div className="flex justify-center items-center gap-10">
+              <input
+                type="text"
+                placeholder="Votre nom"
+                className=" md:px-5 px-3 md:py-2 py-2 rounded-md md:w-full w-full bg-gray-300 text-white outline outline-0 focus:outline-0"
+              />
+              <input
+                type="text"
+                placeholder="Votre nom"
+                className=" md:px-5 px-3 md:py-2 py-2 rounded-md md:w-full w-full bg-gray-300 text-white outline outline-0 focus:outline-0"
+              />
+            </div>
+          </form>
         </div>
+        <p></p>
       </div>
     </>
-  )
+  );
 }
 
 export default Devis
