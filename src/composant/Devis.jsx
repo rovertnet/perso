9d37051e5fd4d14a45ebd2a07f4e8c24b9dfb278
Robert Matundu <matundukabamba@gmail.com>
2024-02-01 16:00:32 +0100

    update

--- src/composant/Devis.jsx
+++ src/composant/Devis.jsx
@@ -10,47 +10,53 @@ function Devis() {
             X
           </button>
         </div>
-        <div className=" mb-10 shadow-lg shadow-zinc-200 bg-white opacity-40 rounded-md md:px-10 px-3 md:py-10 py-5 md:w-full w-full">
-          {/* Formulaire */}
-          <form action="">
-            <div className="flex justify-center items-center gap-2">
-              <input
-                type="text"
-                placeholder="Votre nom"
-                className=" md:px-5 px-3 md:py-2 py-2 rounded-md md:w-full w-full bg-gray-300 text-slate-900 outline outline-0 focus:outline-0"
-              />
-              <input
-                type="text"
-                placeholder="Votre nom"
-                className=" md:px-5 px-3 md:py-2 py-2 rounded-md md:w-full w-full bg-gray-300 text-slate-900 outline outline-0 focus:outline-0"
-              />
-            </div>
-            <div className=" md:my-5 my-3">
-              <input
-                type="mail"
-                placeholder="Votre mail"
-                className=" md:px-5 px-3 md:py-2 py-2 rounded-md md:w-full w-full bg-gray-300 text-slate-900 outline outline-0 focus:outline-0"
-              />
-            </div>
-            <div className="md:my-5 my-3">
-              <textarea
-                name=""
-                id=""
-                cols="20"
-                rows="5"
-                placeholder="Votre message"
-                className="md:px-5 px-3 md:py-2 py-2 rounded-md md:w-full w-full bg-gray-300 text-slate-900 outline outline-0 focus:outline-0"
-              />
-            </div>
-            <div>
-              <button className=" bg-black text-white rounded-md font-semibold text-xl md:px-10 px-10 md:py-3 py-2">
-                {" "}
-                Envoyez
-              </button>
-            </div>
-          </form>
+        {/* Block form */}
+        <div className="flex justify-normal items-center gap-10">
+          <div className="">
+            <img src="" alt="" />
+          </div>
+          {/* le formulaire */}
+          <div className=" mb-10 shadow-lg shadow-zinc-200 bg-white opacity-40 rounded-md md:px-10 px-3 md:py-10 py-5 md:w-full w-full">
+            {/* Formulaire */}
+            <form action="">
+              <div className="flex justify-center items-center gap-2">
+                <input
+                  type="text"
+                  placeholder="Votre nom"
+                  className=" md:px-5 px-3 md:py-2 py-2 rounded-md md:w-full w-full bg-gray-300 text-slate-900 outline outline-0 focus:outline-0"
+                />
+                <input
+                  type="text"
+                  placeholder="Votre nom"
+                  className=" md:px-5 px-3 md:py-2 py-2 rounded-md md:w-full w-full bg-gray-300 text-slate-900 outline outline-0 focus:outline-0"
+                />
+              </div>
+              <div className=" md:my-5 my-3">
+                <input
+                  type="mail"
+                  placeholder="Votre mail"
+                  className=" md:px-5 px-3 md:py-2 py-2 rounded-md md:w-full w-full bg-gray-300 text-slate-900 outline outline-0 focus:outline-0"
+                />
+              </div>
+              <div className="md:my-5 my-3">
+                <textarea
+                  name=""
+                  id=""
+                  cols="20"
+                  rows="5"
+                  placeholder="Votre message"
+                  className="md:px-5 px-3 md:py-2 py-2 rounded-md md:w-full w-full bg-gray-300 text-slate-900 outline outline-0 focus:outline-0"
+                />
+              </div>
+              <div>
+                <button className=" bg-black text-white rounded-md font-semibold text-xl md:px-10 px-10 md:py-3 py-2">
+                  {" "}
+                  Envoyez
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <p></p>
       </div>
     </>
   );
