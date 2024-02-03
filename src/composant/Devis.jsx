7ceab57d4235b9929c3433679cfcf8938f92a8c4
Robert Matundu <matundukabamba@gmail.com>
2024-02-03 15:39:22 +0100

    mise à jour

--- src/composant/Devis.jsx
+++ src/composant/Devis.jsx
@@ -3,27 +3,35 @@ import { IoMailUnread } from "react-icons/io5";
 function Devis() {
   return (
     <>
-      <div className="md:px-10 px-10  md:py-3 py-5 rounded-md bg-cover object-cover bg-[url('../../public/house.jpg')] ">
-        <div className="flex justify-between items-center md:px-10 px-3">
+      <div className="md:px-10 px-10  md:py-3 py-5 bg-cover object-cover bg-[url('../../public/house.jpg')] ">
+        <div className="flex justify-between items-center">
           <button className=" bg-black md:py-2 py-2 md:px-14 px-7 text-white md:my-10 my-8 font-extrabold rounded-md">
             Modal
           </button>
-          <button className=" bg-black md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold rounded">
-            <IoMailUnread className=" text-white font-bold text-xl" />
-          </button>
+          <div className=" flex justify-center items-center gap-3">
+            <button className=" bg-black md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold rounded">
+              <IoMailUnread className=" text-white font-bold text-xl" />
+            </button>
+            <button className=" bg-black md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold rounded">
+              <IoMailUnread className=" text-white font-bold text-xl" />
+            </button>
+            <button className=" bg-black md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold rounded">
+              <IoMailUnread className=" text-white font-bold text-xl" />
+            </button>
+          </div>
         </div>
         {/* Block form */}
         <div className="flex flex-col md:flex-row-reverse justify-normal items-center gap-10">
           <div className=" md:w-5/3 w-5/3 md:px-10 px-3">
             <img
-              src="../public/moi.png"
+              src="../public/CONTACT.png"
               alt=""
               className=" md:w-full rounded-md"
             />
           </div>
           {/* le formulaire */}
           <div className=" md:w-4/5 w-5/3">
-            <div className=" mb-10 shadow-lg bg-[#b0b0b0c4] rounded-md md:px-10 px-8 md:py-10 py-8 md:w-full w-full">
+            <div className=" mb-10 shadow-lg bg-white/5 backdrop-blur-2xl rounded-md md:px-10 px-8 md:py-10 py-8 md:w-full w-full">
               {/* Formulaire */}
               <form action="">
                 <div className="flex md:flex-row flex-col justify-center items-center gap-3">
