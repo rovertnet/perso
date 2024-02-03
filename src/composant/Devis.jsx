3a601df4df9e4f48c2b64158b98e7b014b2415fa
Robert Matundu <matundukabamba@gmail.com>
2024-02-03 14:28:38 +0100

    mise à jour

--- src/composant/Devis.jsx
+++ src/composant/Devis.jsx
@@ -1,18 +1,20 @@
+import { IoMailUnread } from "react-icons/io5";
+
 function Devis() {
   return (
     <>
-      <div className="md:px-10 px-10  md:py-3 py-5 rounded-md bg-[background-] ">
-        <div className="flex justify-between items-center">
+      <div className="md:px-10 px-10  md:py-3 py-5 rounded-md bg-cover object-cover bg-[url('../../public/house.jpg')] ">
+        <div className="flex justify-between items-center md:px-10 px-3">
           <button className=" bg-black md:py-2 py-2 md:px-14 px-7 text-white md:my-10 my-8 font-extrabold rounded-md">
             Modal
           </button>
           <button className=" bg-black md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold rounded">
-            X
+            <IoMailUnread className=" text-white font-bold text-xl" />
           </button>
         </div>
         {/* Block form */}
         <div className="flex flex-col md:flex-row-reverse justify-normal items-center gap-10">
-          <div className=" md:w-2/6">
+          <div className=" md:w-5/3 md:px-10 px-3">
             <img
               src="../public/moi.png"
               alt=""
@@ -21,10 +23,10 @@ function Devis() {
           </div>
           {/* le formulaire */}
           <div className=" md:w-4/5 w-5/3">
-            <div className=" mb-10 shadow-lg shadow-zinc-200 bg-white opacity-40 rounded-md md:px-10 px-3 md:py-10 py-5 md:w-full w-full">
+            <div className=" mb-10 shadow-lg shadow-zinc-200 bg-gray-700 opacity-40 rounded-md md:px-10 px-3 md:py-10 py-5 md:w-full w-full">
               {/* Formulaire */}
               <form action="">
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex md:flex-row flex-col justify-center items-center gap-3">
                   <input
                     type="text"
                     placeholder="Votre nom"
