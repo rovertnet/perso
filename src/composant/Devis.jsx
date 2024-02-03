e213c9a0ff06bbfd7b838180ef87c89a3031bb4f
Robert Matundu <matundukabamba@gmail.com>
2024-02-03 15:59:10 +0100

    mise à jour

--- src/composant/Devis.jsx
+++ src/composant/Devis.jsx
@@ -8,7 +8,7 @@ function Devis() {
           <button className=" bg-black md:py-2 py-2 md:px-14 px-7 text-white md:my-10 my-8 font-extrabold rounded-md">
             Modal
           </button>
-          <div className=" flex justify-center items-center gap-3">
+          <div className=" flex justify-center items-center gap-3 bg-white/5 backdrop-blur-xl md:">
             <button className=" bg-black md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold rounded">
               <IoMailUnread className=" text-white font-bold text-xl" />
             </button>
