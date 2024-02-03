df63851c54637d4cf83992b61d30d5bb3c5aee3e
Robert Matundu <matundukabamba@gmail.com>
2024-02-03 16:38:12 +0100

    mise à jour

--- src/composant/Devis.jsx
+++ src/composant/Devis.jsx
@@ -1,21 +1,23 @@
 import { IoMailUnread } from "react-icons/io5";
+import { FaPhoneAlt } from "react-icons/fa";
+import { RiMapPin2Fill } from "react-icons/ri";
 
 function Devis() {
   return (
     <>
-      <div className="md:px-10 px-10  md:py-3 py-5 bg-cover object-cover bg-[url('../../public/house.jpg')] ">
+      <div className="md:px-10 px-10  md:py-3 py-5 bg-cover object-cover bg-[url('../../public/bg1.jpg')] ">
         <div className="flex justify-between items-center">
           <button className=" bg-black md:py-2 py-2 md:px-14 px-7 text-white md:my-10 my-8 font-extrabold rounded-md">
             Modal
           </button>
-          <div className=" flex justify-center items-center gap-3 bg-white/5 backdrop-blur-xl md:">
-            <button className=" bg-black md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold rounded">
-              <IoMailUnread className=" text-white font-bold text-xl" />
+          <div className=" flex justify-center items-center gap-3 bg-white/35 backdrop-blur-2xl md:px-10 px-3 md:mb-8 mb-6 rounded-md md:py-1 py-0">
+            <button className=" bg-black rounded-md md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold ">
+              <FaPhoneAlt className=" text-white font-bold text-xl" />
             </button>
-            <button className=" bg-black md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold rounded">
-              <IoMailUnread className=" text-white font-bold text-xl" />
+            <button className=" bg-black rounded-md md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold ">
+              <RiMapPin2Fill className=" text-white font-bold text-xl" />
             </button>
-            <button className=" bg-black md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold rounded">
+            <button className=" bg-black rounded-md md:py-2 py-2 md:px-4 p-4 text-white md:my-10 my-8 font-extrabold ">
               <IoMailUnread className=" text-white font-bold text-xl" />
             </button>
           </div>
@@ -31,7 +33,7 @@ function Devis() {
           </div>
           {/* le formulaire */}
           <div className=" md:w-4/5 w-5/3">
-            <div className=" mb-10 shadow-lg bg-white/5 backdrop-blur-2xl rounded-md md:px-10 px-8 md:py-10 py-8 md:w-full w-full">
+            <div className=" mb-10 shadow-lg bg-white/35 backdrop-blur-2xl rounded-md md:px-10 px-8 md:py-10 py-8 md:w-full w-full">
               {/* Formulaire */}
               <form action="">
                 <div className="flex md:flex-row flex-col justify-center items-center gap-3">
